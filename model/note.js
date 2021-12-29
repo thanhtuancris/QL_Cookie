853e4128c9b3502059b258c7a4fba9eec943a59e
thanhtuan <thanhtuancristiano@gmail.com>
2021-12-29 14:43:09 +0700

    update
    update

--- model/note.js
+++ model/note.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const noteSchema = new mongoose.Schema({
 	name: String,
-    date_import: String
+    date_import: Date
 })
 
 module.exports = mongoose.model("notes", noteSchema)
