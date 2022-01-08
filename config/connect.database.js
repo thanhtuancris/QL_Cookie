0811102d9d6c13a33e0711222c71aea693715a23
thanhtuan <thanhtuancristiano@gmail.com>
2022-01-08 15:22:04 +0700

    update
    update

--- config/connect.database.js
+++ config/connect.database.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 async function connect(){
     try{
-        await mongoose.connect('mongodb://admin:admin123456@127.0.0.1:27017/QL_Cookie?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-        });
-        // await mongoose.connect('mongodb://admin:adminXYZdksfn231@127.0.0.1:27017/QL_Cookie?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
+        // await mongoose.connect('mongodb://admin:admin123456@127.0.0.1:27017/QL_Cookie?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
         //     useNewUrlParser: true,
         //     useUnifiedTopology: true,
         //     useFindAndModify: false,
         // });
+        await mongoose.connect('mongodb://admin:adminXYZdksfn231@127.0.0.1:27017/QL_Cookie?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
         console.log('connected to database');
     }catch(e){
         console.log('failed to connect');
