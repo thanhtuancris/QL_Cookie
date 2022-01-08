1f011d8255f7983811f1b6e60324cdf5426359be
thanhtuan <thanhtuancristiano@gmail.com>
2022-01-08 15:48:37 +0700

    update
    update

--- model/account.js
+++ model/account.js
@@ -4,6 +4,9 @@ const dataSchema = new mongoose.Schema({
     username: String,
     password: String,
     token: String,
+    isdelete: Boolean,
+    status: Boolean,
+    role: Number
 })
 
 module.exports = mongoose.model("accounts", dataSchema)
